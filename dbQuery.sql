d4186f560cad9d3dc6529cff97b5f70f4ba92a1f
roshan <roshan@roshan.roshan>
2022-01-14 01:12:59 +0530

    fifa management

--- dbQuery.sql
+++ dbQuery.sql
@@ -1,1 +1,1 @@
-CREATE TABLE `DBMSminiProject`.`PLAYERS` ( `player_id` INT NOT NULL AUTO_INCREMENT , `player_name` VARCHAR(30) NOT NULL , `age` INT(2) NOT NULL , `overall_rating` INT NOT NULL , `nationality` VARCHAR(30) NOT NULL , `in-contract` INT(10) NOT NULL , PRIMARY KEY (`player_id`)) ENGINE = InnoDB;
+CREATE TABLE `DBMSminiProject`.`PLAYERS` ( `id` INT NOT NULL AUTO_INCREMENT , `player_name` VARCHAR(30) NOT NULL , `age` INT(2) NOT NULL , `overall_rating` INT NOT NULL , `nationality` VARCHAR(30) NOT NULL , `contract` INT(10) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
