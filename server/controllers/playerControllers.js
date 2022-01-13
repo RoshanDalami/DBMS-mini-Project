627228ee07cbd89251de75b759a7228813af6229
roshan <roshan@roshan.roshan>
2022-01-14 01:11:45 +0530

    fifa management

--- server/controllers/playerControllers.js
+++ server/controllers/playerControllers.js
@@ -21,7 +21,8 @@ exports.view = (request,response)=>{
             //when done with connection release it
             connection.release();
             if(!err){
-                response.render('index',{rows});
+                let removedPlayer = request.query.removed;
+      response.render('index', { rows, removedPlayer });
             }else{
                 console.log(err)
             }
@@ -36,7 +37,7 @@ exports.find = (request,response)=>{
         console.log('connected to database...')
         let searchTerm = request.body.search;
         //user connection
-        connection.query('SELECT * FROM PLAYERS WHERE player_name LIKE ? OR player_id LIKE ? OR age LIKE ? OR overall_rating LIKE ? OR nationality LIKE ? OR in_contract LIKE ? OR position LIKE ?',['%'+searchTerm+'%','%'+searchTerm+'%','%'+searchTerm+'%','%'+searchTerm+'%','%'+searchTerm+'%','%'+searchTerm+'%','%'+searchTerm+'%'],(err,rows)=>{
+        connection.query('SELECT * FROM PLAYERS WHERE player_name LIKE ? ',['%'+searchTerm+'%'],(err,rows)=>{
             //when done with connection release it
             connection.release();
             if(!err){
@@ -55,13 +56,13 @@ exports.player =(request,response)=>{
 //add player
 exports.addplayer =(request,response)=>{
 //  response.render('addplayer')
-    const {player_name,age,overall_rating,nationality,in_contract,position}=request.body;
+    const {player_name,age,overall_rating,nationality,contract,position}=request.body;
     pool.getConnection((err,connection)=>{
         if(err) throw err; //if error
         console.log('connected to database...')
        
         //user connection
-        connection.query('INSERT INTO PLAYERS SET player_name = ?,age = ?,overall_rating = ?,nationality=?,in_contract=?,position=?',[player_name,age,overall_rating,nationality,in_contract,position],(err,rows)=>{
+        connection.query('INSERT INTO PLAYERS SET player_name = ?,age = ?,overall_rating = ?,nationality=?,contract=?,position=?',[player_name,age,overall_rating,nationality,contract,position],(err,rows)=>{
             //when done with connection release it
             connection.release();
             if(!err){
@@ -69,8 +70,106 @@ exports.addplayer =(request,response)=>{
             }else{
                 console.log(err)
             }
+           
+        });
+    });
+};
+//Edit player
+exports.edit =(request,response)=>{
+    // response.render('editplayer')
+    pool.getConnection((err,connection)=>{
+        if(err) throw err; //if error
+        console.log('connected to database...')
+
+        //user connection
+        connection.query('SELECT * FROM PLAYERS WHERE id = ?',[request.params.id],(err,rows)=>{
+            //when done with connection release it
+            connection.release();
+            if(!err){
+                response.render('editplayer',{rows});
+            }else{
+                console.log(err)
+            }
+            
+        });
+    });
+}
+//update Player
+exports.update =(request,response)=>{
+    const {player_name,age,overall_rating,nationality,contract,position}=request.body;
+    // response.render('editplayer')
+    pool.getConnection((err,connection)=>{
+        if(err) throw err; //if error
+        console.log('connected to database...')
+
+        //user connection
+        connection.query('UPDATE PLAYERS SET player_name=?,age=?,overall_rating=?,nationality=?,contract=?,position=? WHERE id=?',[player_name,age,overall_rating,nationality,contract,position,request.params.id],(err,rows)=>{
+            //when done with connection release it
+            connection.release();
+            if(!err){
+                pool.getConnection((err,connection)=>{
+                    if(err) throw err; //if error
+                    console.log('connected to database...')
+            
+                    //user connection
+                    connection.query('SELECT * FROM PLAYERS WHERE id = ?',[request.params.id],(err,rows)=>{
+                        //when done with connection release it
+                        connection.release();
+                        if(!err){
+                            response.render('editplayer',{rows,alert:`${player_name} has been updated`});
+                        }else{
+                            console.log(err)
+                        }
+                        
+                    });
+                });
+            }else{
+                console.log(err)
+            }
+            
+        });
+    });
+}
+
+
+//delete player
+exports.delete =(request,response)=>{
+    // response.render('editplayer')
+    pool.getConnection((err,connection)=>{
+        if(err) throw err; //if error
+        console.log('connected to database...')
+
+        //user connection
+        connection.query('DELETE FROM PLAYERS WHERE id = ?',[request.params.id],(err,rows)=>{
+            //when done with connection release it
+            connection.release();
+            if(!err){
+                let removedPlayer = encodeURIComponent('User successeflly removed.');
+                response.redirect('/?removed=' + removedPlayer);
+            }else{
+                console.log(err)
+            }
+            
+        });
+    });
+}
+
+
+exports.viewall = (request,response)=>{
+    pool.getConnection((err,connection)=>{
+        if(err) throw err; //if error
+        console.log('connected to database...')
+
+        //user connection
+        connection.query('SELECT * FROM PLAYERS WHERE id = ?',[request.params.id],(err,rows)=>{
+            //when done with connection release it
+            connection.release();
+            if(!err){
+                response.render('viewplayer',{rows});
+            }else{
+                console.log(err)
+            }
             console.log("the data from player:\n",rows)
         });
     });
 };
-
--- server/router/players.js
+++ server/router/players.js
@@ -5,8 +5,16 @@ const router = express.Router();
 router.get('/', playerControllers.view);
 router.post('/', playerControllers.find);
 
+
 router.get('/addplayer', playerControllers.player);
 router.post('/addplayer', playerControllers.addplayer);
+
+router.get('/editplayer/:id',playerControllers.edit);
+router.post('/editplayer/:id',playerControllers.update);
+
+router.get('/:id',playerControllers.delete);
+
+router.get('/viewplayer/:id',playerControllers.viewall);
 
 
 
